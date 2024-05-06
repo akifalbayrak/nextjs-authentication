0ee00dc0082f68bcf9845efe5ef729b2aaa9108c
akif albayrak <akifalbayrak6161@gmail.com>
2024-05-06 12:15:38 +0300

    chore: Update user sign up functionality

--- actions/auth-actions.js
+++ actions/auth-actions.js
@@ -1,7 +1,9 @@
 "use server";
 
+import { createAuthSession } from "@/lib/auth";
 import { hashUserPassword } from "@/lib/hash";
 import { createUser } from "@/lib/user";
+import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 export async function signUp(prevState, formData) {
@@ -24,7 +26,9 @@ export async function signUp(prevState, formData) {
 
     const hashedPassword = hashUserPassword(password);
     try {
-        createUser(email, hashedPassword);
+        const id = createUser(email, hashedPassword);
+        await createAuthSession(id);
+        redirect("/training");
     } catch (error) {
         if (error.code === "SQLITE_CONSTRAINT_UNIQUE") {
             errors.email = "Email already exists";
@@ -32,5 +36,46 @@ export async function signUp(prevState, formData) {
         }
         throw error;
     }
-    redirect("/training");
+}
+
+export async function verifyAuth() {
+    const sessionCookie = cookies().get(lucia.sessionCookieName);
+
+    if (!sessionCookie) {
+        return {
+            user: null,
+            session: null,
+        };
+    }
+    const sessionId = sessionCookie.value;
+
+    if (!sessionId) {
+        return {
+            user: null,
+            session: null,
+        };
+    }
+
+    const result = await lucia.validateSession(sessionId);
+
+    try {
+        if (result.session && result.session.fresh) {
+            const sessionCookie = lucia.createSessionCookie(result.session.id);
+            cookies.set(
+                sessionCookie.name,
+                sessionCookie.value,
+                sessionCookie.attributes
+            );
+        }
+        if (!result.session) {
+            const sessionCookie = lucia.createBlankSessionCookie();
+            cookies.set(
+                sessionCookie.name,
+                sessionCookie.value,
+                sessionCookie.attributes
+            );
+        }
+    } catch (error) {}
+
+    return result;
 }
